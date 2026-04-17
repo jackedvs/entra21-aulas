6c7c1b2221a85b1058f99489a01580f8c1cc5d46
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-04-17 20:08:17 -0300

    adiciona código importante

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Jorge no seu programa também");
     }
 }
